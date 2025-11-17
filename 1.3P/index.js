4e406608c06c9e428d9a41c837c54f2f2fec6c97
NandiniKhosla <s225726299@deakin.edu.au>
2025-11-17 16:43:12 +0530

    adding the required files in the repo

--- 1.3P/index.js
+++ 1.3P/index.js
@@ -1,1 +1,11 @@
-console.log("Hello SIT725! This is task 1.3P");
+function changeText() {
+    var textsArray = ["Text 1", "Text 2", "Text 3", "Text 4", "Text 5"];
+    var number = getRandomNumberBetween(0, textsArray.length - 1);
+
+    console.log("Index:", number);
+    document.getElementById("heading").innerHTML = textsArray[number];
+}
+
+function getRandomNumberBetween(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
